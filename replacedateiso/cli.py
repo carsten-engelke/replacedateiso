b8225003adf8def8ebffb28217e6038d0de4cc39
Carsten Engelke <carsten.engelke@gmail.com>
2023-12-23 17:47:01 +0100

    repaired script

--- replacedateiso/cli.py
+++ replacedateiso/cli.py
@@ -19,12 +19,12 @@ def main():  # pragma: no cover
     """
 
     for file in Path.cwd().iterdir():
-    match = re.search('\d{2}.\d{2}.\d{4}', file.name)
-    if match != None:
-        dateparam = match.group().split(".")
-        day = dateparam[0]
-        month = dateparam[1]
-        year = dateparam[2]
-        newname = file.name.replace(match.group(), year + "-" + month + "-" + day)
-        print("Found Date:" + file.name + " [ " + match.group() + " ] -> " + newname)
-        os.rename(file, file.parent / newname)
+        match = re.search('\d{2}.\d{2}.\d{4}', file.name)
+        if match != None:
+            dateparam = match.group().split(".")
+            day = dateparam[0]
+            month = dateparam[1]
+            year = dateparam[2]
+            newname = file.name.replace(match.group(), year + "-" + month + "-" + day)
+            print("Found Date:" + file.name + " [ " + match.group() + " ] -> " + newname)
+            os.rename(file, file.parent / newname)
